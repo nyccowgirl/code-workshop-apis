95949a036a6558997512cd98e117cfaba9f2116e
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-18 20:49:47 -0700

    after class

--- api-call.py
+++ api-call.py
@@ -5,15 +5,15 @@ from config import ACCOUNT_SID, AUTH_TOKEN, TWILIO_NUMBER
 
 # add one of you phone numbers here with no spaces
 # exampe: "+15553334444"
-to_number = "+1##########"
+to_number = "+---------"
 
 # set a variable call client to an instance of Client
-client = #your code
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
 # lets make and send the message, add in the missing data
-message = #your code on this line(
-                to=to_number,
-                from_=TWILIO_NUMBER,
-                body="You did it!")
+message = client.api.account.messages.create(
+        to=to_number,
+        from_=TWILIO_NUMBER,
+        body="Twilio thinks you are Trang")
 
 print "Message set. Message Id: {}".format(message.sid)
--- send_sms.py
+++ send_sms.py
@@ -4,20 +4,24 @@ from config import ACCOUNT_SID, AUTH_TOKEN, TWILIO_NUMBER
 
 def validates_choice(user_choice):
     """Returns bool of user's choice"""
-
-    pass
+    if user_choice == "yes" or "yah":
+        return True
+    else:
+        return False
+   
 
 
 def gets_to_number():
     """Ask user for phone number, returns it with +1 added"""
 
-    pass
-
+    to_number= "+1" + raw_input("what number do you want to sext?")
+    return to_number
 
 def gets_message_to_send():
     """Ask user for message to send"""
 
-    pass
+    to_message = raw_input("Say something sessy!")
+    return to_message
 
 
 def send_message(to_number, to_message, ACCOUNT_SID,
@@ -38,7 +42,7 @@ def main(ACCOUNT_SID, AUTH_TOKEN, TWILIO_NUMBER):
 
     print "Welcome to this Twilio API Demo"
 
-    user_choice = raw_input("""Would you like to send a next message? """)
+    user_choice = raw_input("""Would you like to send a sext message? """)
 
     validated_user_choice = validates_choice(user_choice)
 
@@ -50,7 +54,7 @@ def main(ACCOUNT_SID, AUTH_TOKEN, TWILIO_NUMBER):
         send_message(to_number, to_message, ACCOUNT_SID,
                             AUTH_TOKEN, TWILIO_NUMBER)
 
-        user_choice = raw_input("""Would you like to send another next message? """)
+        user_choice = raw_input("""Would you like to send another sext message? """)
 
         validated_user_choice = validates_choice(user_choice)
 
